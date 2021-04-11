1fb38608fbe59cf2ed8901ad1cba5c5f93008764
Florian <convert@etud.insa-toulouse.fr>
2021-04-11 19:03:25 +0200

    fin episode 2 eric evenement wifi

--- Exemple_wifi_evenements_esp8266/src/main.cpp
+++ Exemple_wifi_evenements_esp8266/src/main.cpp
@@ -7,6 +7,10 @@
 const char * SSID = "SFR_EAC0";
 const char * PASSWORD = "gaz_de_shciste";
 
+//Gestion des évènements du Wifi
+void onConnected(const WiFiEventStationModeConnected& event);
+void onGotIP(const WiFiEventStationModeGotIP& event);
+
 
 
 void setup() {
@@ -19,6 +23,9 @@ void setup() {
 
   //Démarrage de la connection:
   WiFi.begin(SSID, PASSWORD);
+  static WiFiEventHandler onConnectedHandler = WiFi.onStationModeConnected(onConnected);
+  static WiFiEventHandler onGotIPHandler = WiFi.onStationModeGotIP(onGotIP);
+
 
   //configuration des E/S
   pinMode(PIN_LED_ROUGE, OUTPUT);
@@ -30,14 +37,24 @@ void loop() {
 
   if (WiFi.isConnected()){
     digitalWrite(PIN_LED_ROUGE, HIGH);
-    Serial.println("adresse IP : " + WiFi.localIP().toString());
-    Serial.println("adresse passerelle (gateway) : " + WiFi.gatewayIP().toString());
-    Serial.println("adresse DNS : " + WiFi.dnsIP().toString());
-    Serial.print("Puissance de réception du signal : " );
-    Serial.println(WiFi.RSSI());
+    
   }
   else {
     digitalWrite(PIN_LED_ROUGE, LOW);
   }
 
+}
+
+void onConnected(const WiFiEventStationModeConnected& event){
+  Serial.println("Wifi connectée");
+  Serial.println("adresse IP : " + WiFi.localIP().toString());
+}
+
+
+void onGotIP(const WiFiEventStationModeGotIP& event){
+  Serial.println("adresse IP : " + WiFi.localIP().toString());
+  Serial.println("adresse passerelle (gateway) : " + WiFi.gatewayIP().toString());
+  Serial.println("adresse DNS : " + WiFi.dnsIP().toString());
+  Serial.print("Puissance de réception du signal : " );
+  Serial.println(WiFi.RSSI());
 }
